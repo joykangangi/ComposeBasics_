41c2f92da1fd51be34561e8d893297fb75f7f980
joykangangi <64706463+joykangangi@users.noreply.github.com>
2022-02-08 21:02:18 +0300

    State Hoisting, rememberSaveable, animation using low-level api

--- ComposeBasics/app/src/main/java/com/example/composebasics/MainActivity.kt
+++ ComposeBasics/app/src/main/java/com/example/composebasics/MainActivity.kt
@@ -3,16 +3,23 @@ package com.example.composebasics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composebasics.ui.theme.ComposeBasicsTheme
+import com.google.android.material.color.MaterialColors
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,8 +35,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 private fun Greeting(name: String) {
-    val expanded = remember { mutableStateOf(false) }
-    val extraPadding = if (expanded.value)48.dp else 0.dp
+    var expanded by rememberSaveable { mutableStateOf(false) }
+    val extraPadding by animateDpAsState(
+        if (expanded)48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
+    val changeColor = if (expanded) Color.Yellow else Color.White
+
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
@@ -37,32 +52,69 @@ private fun Greeting(name: String) {
         Row(Modifier.padding(24.dp)) {
             Column(modifier = Modifier
                 .weight(1f)
-                .padding(bottom = extraPadding)) {
-            Text(text = "Hello,")
-            Text(text = "$name!") 
+                .padding(bottom = extraPadding)
+                .background(changeColor)) {
+                Text(text = "Hello,")
+                Text(text = "$name!")
             }
             OutlinedButton(
-                onClick = { expanded.value = !expanded.value },
+                onClick = { expanded = !expanded }
             ) {
-                Text(text = if (expanded.value ) "Show  Less" else "Show More")
+                Text(text = if (expanded ) "Show  Less" else "Show More")
             }
         }
     }
 }
 
 @Composable
-private fun MyApp(names: List<String> = listOf("Gideon", "Sharon", "Joy")) {
-       Column(modifier = Modifier.padding(vertical = 4.dp)) {
-           for (name in names) {
-               Greeting(name = name)
-           }
-       }
+private fun MyApp() {
+    var shouldShowOnBoarding by rememberSaveable {
+        mutableStateOf(true)
     }
+    if (shouldShowOnBoarding) {
+        OnBoardingScreen(onContinueClicked = {shouldShowOnBoarding = false})
+    } else {
+        Greetings()
+    }
+}
+@Composable
+private fun Greetings(names: List<String> = List(1000){"$it"}) {
+    //here $it represents the list index
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
+            Greeting(name = name)
+        }
+    }
+}
+
+
+@Composable
+private fun OnBoardingScreen(onContinueClicked:() -> Unit) {
+
+    Column(
+        Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,//align column contents at the centre
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = "Welcome to the basics Codelab")
+        Button(
+            onClick = {  },
+            modifier = Modifier.padding(vertical = 24.dp)
+        ) {
+            Text("Continue")
+        }
+    }
+}
+/**
+By passing a function and not a state to OnboardingScreen we are making
+this composable more reusable and protecting the state from being
+mutated by other composables.
+ */
 
 @Preview(showBackground = true)
 @Composable
 private fun DefaultPreview() {
     ComposeBasicsTheme {
-       MyApp()
+        OnBoardingScreen(onContinueClicked = {})
+        MyApp()
     }
 }
